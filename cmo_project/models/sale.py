188668507e78ce50e9cb377d56459dc23395f493
Siraparn <siraparn@trinityroots.co.th>
2022-05-08 12:05:26 +0700

    Update sale.py

--- cmo_project/models/sale.py
+++ cmo_project/models/sale.py
@@ -6,17 +6,17 @@ from openerp import fields, models, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    # @api.multi
-    # @api.depends('order_line.price_subtotal')
-    # def _amount_all(self):
-    #     res = super(SaleOrder, self)._amount_all()
-    #     for order in self:
-    #         if order.project_related_id:
-    #             project_id = order.project_related_id
-    #             project_id.date_last_modify = fields.Datetime.now()
-    #     return res
+    @api.multi
+    @api.depends('order_line.price_subtotal')
+    def _amount_all(self):
+        res = super(SaleOrder, self)._amount_all()
+        for order in self:
+            if order.project_related_id:
+                project_id = order.project_related_id
+                project_id.date_last_modify = fields.Datetime.now()
+        return res
 
-    @api.onchange('state', 'amount_total')
-    def _onchange_project_track(self):
-        if self.project_related_id:
-            self.project_related_id.date_last_modify = fields.Datetime.now()
+    # @api.onchange('state', 'amount_total')
+    # def _onchange_project_track(self):
+    #     if self.project_related_id:
+    #         self.project_related_id.date_last_modify = fields.Datetime.now()
